c0c5662e347ae07fe083cbc789ce3475a55289f4
Naratip Boonbanyen <naratip.boo@axonstech.com>
2024-08-03 16:16:43 +0700

    add : combine code in yolov8, update : code other utils to be ezy to use

--- yolov8/validate_model.py
+++ yolov8/validate_model.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 import os
 
-model = YOLO('./asset/cctv-feed-ml_v3-post-prd-022024_yolov8l.pt')
+model = YOLO('../asset/cctv-feed-ml_v3-post-prd-022024_yolov8l.pt')
 
-list_step = [0.5, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85]
+list_step = [0.5, 0.6, 0.7, 0.8]
 
 for x in list_step:
     validation_results = model.val(data='./assets/val.yaml',conf=x)
